bb2ab5016455fa362a8c159972ef022d0d235f74
enz <enzo_naguiat@yahoo.com>
2020-05-14 23:52:28 +0900

    add a create action for the posts api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::PostsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:create]
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def create
+    post = Post.build(post_params)
+    post.user = current_user
+    post.save
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 
 end
