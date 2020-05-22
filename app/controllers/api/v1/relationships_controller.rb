99cab930e7368f658ff4ab042f695c03ccb32204
enz <enzo_naguiat@yahoo.com>
2020-05-22 23:54:28 +0900

    add relationship model and controller for following feature

---
+++ app/controllers/api/v1/relationships_controller.rb
@@ -0,0 +1,18 @@
+class Api::v1::RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    relationship = Relationship.new(followed_id: params[:user_id])
+    relationship.follower_id = current_user.id
+    if relationship.save
+      render json: relationship
+    end
+  end
+
+  def destroy
+    relationship = Relationship.find_by(followed_id: params[:user_id], follower_id: current_user.id)
+    if relationship.destroy
+      render json: relationship
+    end
+  end
+end
